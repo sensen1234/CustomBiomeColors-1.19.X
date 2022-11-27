d1f5f253a3c7d76526d4884a60638eddb955f7bc
sensen1234 <63025571+sensen1234@users.noreply.github.com>
2022-11-27 11:18:35 +0800

    Delete utils directory

--- utils/src/main/java/me/arthed/custombiomecolors/utils/ColorUtils.java
+++
@@ -1,9 +0,0 @@
-package me.arthed.custombiomecolors.utils;
-
-public class ColorUtils {
-
-    public static String intToHex(int color) {
-        return String.format("#%06X", (0xFFFFFF & color));
-    }
-
-}
